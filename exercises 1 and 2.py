c06a56e71863594835b5b9ffae6740890d9d3680
jimward1963 <36490649+jimward1963@users.noreply.github.com>
2018-04-07 19:30:37 +0100

    Add files via upload

---
+++ exercises 1 and 2.py
@@ -0,0 +1,62 @@
+# Ian McLoughlin
+# A program that displays Fibonacci numbers.
+
+def fib(n):
+  """This function returns the nth Fibonacci number."""
+  i = 0
+  j = 1
+  n = n - 1
+
+  while n >= 0:
+    i, j = j, i + j
+    n = n - 1
+  
+  return i
+
+# Test the function with the following value.
+x = 29
+ans = fib(x)
+print("Fibonacci number", x, "is", ans)
+
+
+
+
+# Ian McLoughlin
+# A program that displays Fibonacci numbers using people's names.
+
+def fib(n):
+  """This function returns the nth Fibonacci number."""
+  i = 0
+  j = 1
+  n = n - 1
+
+  while n >= 0:
+    i, j = j, i + j
+    n = n - 1
+  
+  return i
+
+name = "Ward"
+first = name[0]
+last = name[-1]
+firstno = ord(first)
+lastno = ord(last)
+x = firstno + lastno
+
+ans = fib(x)
+print("My surname is", name)
+print("The first letter", first, "is number", firstno)
+print("The last letter", last, "is number", lastno)
+print("Fibonacci number", x, "is", ans)
+
+
+
+#Hi Ian,
+#My name is James and the first letter has value of 10 and the second letter has value of 19 giving a total of 29.
+#The Fibonacci number for 29 is 514229. Program code attached.
+
+
+#Ord () gives the Unicode value of the letters in the brackets.
+#If you can figure out what went wrong, please let me know.
+#Regards
+#Jim
